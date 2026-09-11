4a5b8e2c0df31067d88dd005866ee11e6b05ab9e
Talha Javed <64962278+talhajavedawan@users.noreply.github.com>
2026-09-11 15:10:23 +0100

    security: remove hard-coded SQL fallback passwords

--- ERP_BL/Config/Config.cs
+++ ERP_BL/Config/Config.cs
@@ -9,53 +9,35 @@ using ERP_BL.Enums;
 
 namespace ERP_BL.Config
 {
-
     public class Config : IConfig
     {
-        string connectedDB=null;
-        string connectedServer= null;
+        string connectedDB = null;
+        string connectedServer = null;
 
         private Server server = new Server();
 
-
-        /// <summary>
-        /// this will keep Database name.
-        /// </summary>
         public string groupName
         {
             get { return this.connectedDB; }
             set { this.connectedDB = value; }
         }
 
-        /// <summary>
-        /// this will keep Server name connected.
-        /// </summary>
         public string serverName
         {
             get { return this.connectedServer; }
             set { this.connectedServer = value; }
         }
 
-        /// <summary>
-        /// this will return all industry types supported
-        /// </summary>
-        /// <returns></returns>
         public List<string> getIndustryType()
         {
-            List<string> list= new List<string>();
-            //IndustryTypes indt = new IndustryTypes();
+            List<string> list = new List<string>();
             list.Add(IndustryTypes.ServiceProvider.ToString());
-
             list.Add(IndustryTypes.InformationTechnology.ToString());
             list.Add(IndustryTypes.Construction.ToString());
             ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "list of Industry Types is Retrived");
-
             return list;
         }
-        /// <summary>
-        /// this will return all Inquiry types supported
-        /// </summary>
-        /// <returns></returns>
+
         public List<string> getInquiryType()
         {
             List<string> list = new List<string>();
@@ -69,13 +51,9 @@ namespace ERP_BL.Config
             list.Add(InquiryType.SupplyCCC.ToString());
             list.Add(InquiryType.DistributionBiz_CustomerCredit.ToString());
             ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "list of Inquiry Types is Retrived");
-
             return list;
         }
-        /// <summary>
-        /// this will return all Inquiry types supported
-        /// </summary>
-        /// <returns></returns>
+
         public List<string> getPurchaseOrderTypes()
         {
             List<string> list = new List<string>();
@@ -86,27 +64,21 @@ namespace ERP_BL.Config
             list.Add(InquiryType.Standard.ToString());
             list.Add(InquiryType.Inventory.ToString());
             list.Add(InquiryType.DistributionBiz.ToString());
-
             ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "list of Inquiry Types is Retrived");
-
             return list;
         }
-        /// <summary>
-        /// this will return all Business types supported
-        /// </summary>
-        /// <returns></returns>
+
         public List<string> getBizType()
         {
             List<string> list = new List<string>();
             list.Add(BizTypes.Corporation.ToString());
-            
             list.Add(BizTypes.Partnership.ToString());
             list.Add(BizTypes.SoleProprietor.ToString());
             list.Add(BizTypes.NGO.ToString());
             ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "list of BizTypes is Retrived");
-
             return list;
         }
+
         public List<string> getGenderType()
         {
             List<string> list = new List<string>();
@@ -115,53 +87,46 @@ namespace ERP_BL.Config
             list.Add(Gender.Shemale.ToString());
             list.Add(Gender.Not_Disclosed.ToString());
             ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "list of gender Types is Retrived");
-
             return list;
         }
+
         public List<string> getItemNature()
         {
             List<string> list = new List<string>();
             list.Add(ItemNature.Inventory.ToString());
             list.Add(ItemNature.Services.ToString());
-            
             return list;
         }
-        /// <summary>
-        /// Get all active SQL server instances over LAN
-        /// </summary>
-        /// <param name="machineName"></param>
-        /// <returns></returns>
-        public List<string> getAllSQLServer(string machineName )
+
+        public List<string> getAllSQLServer(string machineName)
         {
             return server.localServers(machineName);
-                
         }
 
-        /// <summary>
-        /// Get all associated Databases names in selected server
-        /// </summary>
-        /// <param name="serverName">Name of the server</param>
-        /// <param name="winAuth">windows authentication for server login or not</param>
-        /// <returns></returns>
         public List<string> getDBListAssociated(string serverName, bool winAuth)
         {
             List<string> list = new List<string>();
+            string sqlPassword = Environment.GetEnvironmentVariable("SMARTERP_SQL_PASSWORD");
 
-            list = server.connectLANServer(serverName, winAuth, "Zas367@");
-            if(list==null)
-                list = server.connectLANServer(serverName, winAuth, "sa");
-            if(list==null)
-                ERP_BL.Databases.SystemLog.LogError(this.GetType(), "Error Occured connecting to server " + serverName );
+            if (!winAuth && string.IsNullOrWhiteSpace(sqlPassword))
+            {
+                ERP_BL.Databases.SystemLog.LogError(this.GetType(), "SQL authentication requested but SMARTERP_SQL_PASSWORD is not configured.");
+                return list;
+            }
+
+            list = server.connectLANServer(serverName, winAuth, sqlPassword);
+            if (list == null)
+            {
+                ERP_BL.Databases.SystemLog.LogError(this.GetType(), "Error Occured connecting to server " + serverName);
+            }
             else
             {
                 this.serverName = serverName;
-                ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "Retrived list of Databases associated with Server name= " + serverName + "");
-
+                ERP_BL.Databases.SystemLog.LogInfo(this.GetType(), "Retrived list of Databases associated with Server name= " + serverName);
             }
 
             return list;
         }
-
 
         public List<pUser> connectDB(string DBName)
         {
@@ -170,40 +135,28 @@ namespace ERP_BL.Config
             if (connSuccess)
             {
                 connectedDB = DBName;
-                // load user names and passwords into datatabale
                 list = server.getAllLogins(connectedServer, connectedDB);
-
             }
-
             return list;
-
         }
-    
-    public List<pUser> connectDB(string DBName, string systemVersion)
+
+        public List<pUser> connectDB(string DBName, string systemVersion)
         {
             List<pUser> list = null;
             try
             {
-           
-            bool connSuccess= server.connectDB(this.serverName,DBName, systemVersion);
-            if (connSuccess)
-            {
-                connectedDB = DBName;
-                // load user names and passwords into datatabale
-                list = server.getAllLogins(connectedServer, connectedDB);
-
+                bool connSuccess = server.connectDB(this.serverName, DBName, systemVersion);
+                if (connSuccess)
+                {
+                    connectedDB = DBName;
+                    list = server.getAllLogins(connectedServer, connectedDB);
+                }
             }
-                
-            }
-            catch (Exception ex)
+            catch (Exception)
             {
-                throw ex;
+                throw;
             }
             return list;
-
-
         }
     }
-   
-
 }
